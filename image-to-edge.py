b8f9d19c91fcc0fe815e108cd2abd3fbbecfa0d6
lavnishhh <82657384+lavnishhh@users.noreply.github.com>
2021-12-25 23:08:05 +0530

    Added file

---
+++ image-to-edge.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy
+
+vid = cv2.VideoCapture(0)
+render_resolution=300
+vid.set(3,render_resolution)
+vid.set(2,(render_resolution/4)*3)
+
+ret, frame = vid.read()
+img = numpy.ones([frame.shape[0],frame.shape[1]])
+while(True):
+        ret, frame = vid.read()
+        frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('vid',frame)
+        for a in range(1,len(frame)-1):
+                for b in range(1,frame.shape[1]-1):
+                        img[a][b]= frame[a][b-1]- 2*frame[a][b] + frame[a][b+1] + frame[a-1][b]- 2*frame[a][b] + frame[a+1][b+1]
+        cv2.imshow('frame', numpy.absolute(img/255.0))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+vid.release()
+cv2.destroyAllWindows()
